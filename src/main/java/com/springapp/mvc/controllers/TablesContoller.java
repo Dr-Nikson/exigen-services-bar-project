b23fe6f2ce7dd950472173fcdc86cfa7c1a7b1e7
Vlad <kutumovvlad@gmail.com>
2014-07-08 11:27:20 +0400

    Controllers

---
+++ src/main/java/com/springapp/mvc/controllers/TablesContoller.java
@@ -0,0 +1,8 @@
+package com.springapp.mvc.controllers;
+
+/**
+ * Created by Vlad on 08.07.2014.
+ */
+public class TablesContoller {
+
+}
---
+++ src/main/java/com/springapp/mvc/controllers/UsersController.java
@@ -0,0 +1,9 @@
+package com.springapp.mvc.controllers;
+
+/**
+ * Created by Vlad on 08.07.2014.
+ */
+public class UsersController {
+
+
+}
