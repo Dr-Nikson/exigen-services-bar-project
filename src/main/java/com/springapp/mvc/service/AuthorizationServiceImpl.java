b18c5144c5c24d94234fc85dcf77a6c953d50684
TofiBashers <tofibashers@gmail.com>
2014-07-08 10:49:20 +0400

    implemented Authorization and Registration services added saving user added authorization controller

---
+++ src/main/java/com/springapp/mvc/service/AuthorizationServiceImpl.java
@@ -0,0 +1,67 @@
+package com.springapp.mvc.service;
+
+import com.springapp.mvc.DAO.UserDAO;
+import com.springapp.mvc.exceptions.AuthorizationException;
+import com.springapp.mvc.model.User;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by TofixXx on 07.07.2014.
+ */
+public class AuthorizationServiceImpl implements AuthorizationService {
+    /**
+     * Метод авторизует пользователя:
+     * 1) Сохраняет ключ авторизации в http-сессии
+     * 2) Сохраняет ключ авторизации в cookies, с флагом http-only-cookies
+     *
+     * @param user Пользователь для авторизации.
+     * @return возвращает авторизованного пользователя
+     * @throws AuthorizationException в случае неудачи
+     */
+    HttpSession session;
+    HttpServletResponse response;
+    public AuthorizationServiceImpl(HttpSession session, HttpServletResponse response)
+    {
+        this.session = session;
+        this.response = response;
+    }
+    public User authorizeUser(User user) throws AuthorizationException
+    {
+        UserDAO userDAO = new UserDAO();
+        User authorizedUser = userDAO.get(user.getEmail(), user.getPassword());
+        if(authorizedUser != null)
+        {
+            session.setAttribute("user", user);
+            session.setAttribute("role", UserRoles.USER);
+            response.addCookie(new Cookie("id", user.getId().toString()));
+        }
+        else
+        {
+            throw new AuthorizationException();
+        }
+        return authorizedUser;
+    }
+
+    /**
+     * Метод проверяет права авторизованного пользователя
+     *
+     * @param role права
+     * @return true, если права у пользователя есть
+     * @throws AuthorizationException если пользователь не авторизован
+     */
+    public boolean checkAccess(UserRoles role) throws AuthorizationException
+    {
+            UserRoles UserRole = (UserRoles)session.getAttribute("role");
+            if(UserRole == role)
+            {
+                return true;
+            }
+            else
+            {
+                throw new AuthorizationException();
+            }
+    }
+}
