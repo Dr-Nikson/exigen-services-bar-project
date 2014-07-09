5e5ac269850392b487eb83449958ec9baf120976
Andrey <ajestkov@gmail.com>
2014-07-09 12:19:05 +0400

    add order in controller

--- src/main/java/com/springapp/mvc/controllers/OrderController.java
+++ src/main/java/com/springapp/mvc/controllers/OrderController.java
@@ -1,7 +1,10 @@
 package com.springapp.mvc.controllers;
 
 import com.springapp.mvc.exceptions.AuthorizationException;
+import com.springapp.mvc.exceptions.OrderException;
 import com.springapp.mvc.json_protocol.JSONResponse;
+import com.springapp.mvc.model.Order;
+import com.springapp.mvc.model.User;
 import com.springapp.mvc.service.AuthorizationService;
 import com.springapp.mvc.service.Impl.AuthorizationServiceImpl;
 import com.springapp.mvc.service.Impl.OrderServiceImpl;
@@ -13,8 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -45,7 +50,7 @@ public class OrderController {
         }
         return responseService.successResponse(orderService.getOrders());
     }*/
-    @RequestMapping(value = "/api/orders/get", method = RequestMethod.POST)
+    @RequestMapping(value = "/api/orders/get", method = RequestMethod.GET)
     public
     @ResponseBody
     JSONResponse getOrdersJson(HttpServletRequest request)
@@ -65,6 +70,33 @@ public class OrderController {
         }
 
         return responseService.successResponse(orderService.getOrders());
+
+    }
+
+
+    @RequestMapping(value = "/api/orders/add", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    JSONResponse addOrdersJson(Date startTime, int personsNum, boolean allRestaurant, String note, boolean ownAlcohol, User user, HttpServletResponse response)
+    {
+        authorizationService = new AuthorizationServiceImpl();
+        responseService = new ResponseServiceImpl();
+        orderService = new OrderServiceImpl();
+
+        Order order = new Order();
+        order.setStartTime(startTime);
+        order.setPersonsNum(personsNum);
+        order.setAllRestaurant(allRestaurant);
+        order.setNote(note);
+        order.setOwnAlcohol(ownAlcohol);
+        order.setUser(user);
+        try {
+            orderService.addOrder(order);
+        } catch (OrderException e) {
+            return responseService.errorResponse("order.not_enough_space", new String(orderService.getFreeSpace(startTime) + ""));
+        }
+        return responseService.successResponse(order);
+
 
     }
 
